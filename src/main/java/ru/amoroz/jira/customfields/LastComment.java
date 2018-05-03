7cee009093ed4126b19e20ab32cb19063eff3219
alx <qm5zp2@gmail.com>
2018-05-03 11:54:15 +0300

    fix naming

---
+++ src/main/java/ru/amoroz/jira/customfields/LastComment.java
@@ -0,0 +1,38 @@
+package ru.amoroz.jira.customfields;
+
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.issue.comments.Comment;
+import com.atlassian.jira.issue.customfields.impl.CalculatedCFType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
+import com.atlassian.jira.issue.Issue;
+import com.atlassian.jira.issue.fields.CustomField;
+
+//@Scanned
+public class LastComment extends CalculatedCFType {
+    private static final Logger log = LoggerFactory.getLogger(LastComment.class);
+
+//    for SortableCustomField interface only
+//
+//    @Override
+//    public int compare(Object str1, Object str2, FieldConfig fieldConfig) {
+//        return ((String) str1).compareTo(((String) str2));
+//    }
+
+    @Override
+    public Object getValueFromIssue(CustomField customField, Issue issue) {
+        Comment c = ComponentAccessor.getCommentManager().getLastComment(issue);
+        return c == null ? "" : (c.getBody().length() > 90 ? c.getBody().substring(0, 90) : c.getBody());
+    }
+
+    @Override
+    public String getStringFromSingularObject(Object o) {
+        return o.toString();
+    }
+
+    @Override
+    public Object getSingularObjectFromString(String s) throws FieldValidationException {
+        return s;
+    }
+}
--- src/main/java/ru/amoroz/jira/customfields/LastCommentedUser.java
+++ src/main/java/ru/amoroz/jira/customfields/LastCommentedUser.java
@@ -3,20 +3,14 @@ package ru.amoroz.jira.customfields;
 import com.atlassian.jira.component.ComponentAccessor;
 import com.atlassian.jira.issue.Issue;
 import com.atlassian.jira.issue.comments.Comment;
-import com.atlassian.jira.issue.comparator.ApplicationUserBestNameComparator;
-import com.atlassian.jira.issue.customfields.SortableCustomField;
-import com.atlassian.jira.issue.customfields.impl.AbstractCustomFieldType;
 import com.atlassian.jira.issue.customfields.impl.CalculatedCFType;
 import com.atlassian.jira.issue.customfields.impl.FieldValidationException;
-import com.atlassian.jira.issue.customfields.view.CustomFieldParams;
 import com.atlassian.jira.issue.fields.CustomField;
-import com.atlassian.jira.issue.fields.config.FieldConfig;
-import com.atlassian.jira.user.ApplicationUser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class LastUserCommentedCFType extends CalculatedCFType {
-    private static final Logger log = LoggerFactory.getLogger(LastUserCommentedCFType.class);
+public class LastCommentedUser extends CalculatedCFType {
+    private static final Logger log = LoggerFactory.getLogger(LastCommentedUser.class);
 
 //    for SortableCustomField interface only
 //
